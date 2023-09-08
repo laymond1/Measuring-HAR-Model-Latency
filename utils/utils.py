e277ece0fe0bd15080c4d2bf8e5a8513da6a8dbb
laymond1 <costor107@gmail.com>
2023-09-09 04:31:46 +0900

    updated utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,7 @@
+import os
+import shutil
+import numpy as np
+
 class AverageMeter(object):
     """
     Computes and stores the average and current value
@@ -22,3 +26,41 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
         
+        
+def create_exp_dir(path, scripts_to_save=None):
+  if not os.path.exists(path):
+    os.makedirs(path)
+  print('Experiment dir : {}'.format(path))
+
+  if scripts_to_save is not None:
+    if not os.path.exists(os.path.join(path, 'scripts')):
+      os.mkdir(os.path.join(path, 'scripts'))
+    for script in scripts_to_save:
+      dst_file = os.path.join(path, 'scripts', os.path.basename(script))
+      shutil.copyfile(script, dst_file)
+
+
+def create_exp_dir(path, scripts_to_save=None):
+  if not os.path.exists(path):
+    os.makedirs(path)
+  print('Experiment dir : {}'.format(path))
+
+  if scripts_to_save is not None:
+    if not os.path.exists(os.path.join(path, 'scripts')):
+      os.mkdir(os.path.join(path, 'scripts'))
+    for script in scripts_to_save:
+      dst_file = os.path.join(path, 'scripts', os.path.basename(script))
+      shutil.copyfile(script, dst_file)
+
+
+def calculate_model_size(model):
+  param_size = 0
+  for param in model.parameters():
+      param_size += param.nelement() * param.element_size()
+  buffer_size = 0
+  for buffer in model.buffers():
+      buffer_size += buffer.nelement() * buffer.element_size()
+
+  size_all_mb = (param_size + buffer_size) / 1024**2
+  
+  return size_all_mb
