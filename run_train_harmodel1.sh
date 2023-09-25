c9763507fdc9c7bed8b2ae25cc1388865a116518
laymond1 <costor107@gmail.com>
2023-09-25 11:09:04 +0900

    added all bash scripts

---
+++ run_train_harmodel1.sh
@@ -0,0 +1,6 @@
+# har model
+for i in $(seq 1 10); do
+    for model in RTWCNN HARLSTM HARBiLSTM HARConvLSTM ResNetTSC FCNTSC; do
+        CUDA_VISIBLE_DEVICES=1 python train_harmodel.py --dataset uci --arch $model --seed $i --trial $i
+    done
+done
