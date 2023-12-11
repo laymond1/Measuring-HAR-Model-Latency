5f44fce65c2997b48659fbfdaf50f87c59bdced2
laymond1 <costor107@gmail.com>
2023-12-12 06:08:06 +0900

    update deleted files

--- run_harnas/run_train_dnas_harnas.sh
+++ run_harnas/run_train_dnas_harnas.sh
@@ -1,6 +1,6 @@
 # Description: Run training for HARNAS on UCI dataset
-for i in $(seq 2 10); do
+for i in $(seq 1 10); do
     for data in uci uni wis opp kar; do
-        CUDA_VISIBLE_DEVICES=0 python ./dnas_harnas/train.py --dataset $data --arch OPPA31 --classifier LSTM --seed $i --trial $i
+        CUDA_VISIBLE_DEVICES=0 python train_dnas_harnas.py --dataset $data --arch OPPA31 --classifier LSTM --seed $i --trial $i
     done
 done
