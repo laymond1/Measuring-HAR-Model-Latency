7f5580486d2ebb1732dd72e345a93f9e5fe1c2ab
laymond1 <costor107@gmail.com>
2023-12-12 06:18:44 +0900

    update run_train_rl_harnas bash

--- run_harnas/run_train_rl_harnas.sh
+++ run_harnas/run_train_rl_harnas.sh
@@ -1,6 +1,6 @@
 # Description: Run training for HARNAS on UCI dataset
 for i in $(seq 1 10); do
     for data in uci uni wis opp kar; do
-        CUDA_VISIBLE_DEVICES=1 python ./rl_harnas/train.py --dataset $data --arch RLNAS --seed $i --trial $i
+        CUDA_VISIBLE_DEVICES=1 python train_rl_harnas.py --dataset $data --arch RLNAS --seed $i --trial $i
     done
 done
